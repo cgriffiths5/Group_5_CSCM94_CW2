145ffd51314bd7f0df6572c31c6d64acb0e39c00
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-11 12:09:16 +0000

    Added login and order page

---
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/LogInController.java
@@ -0,0 +1,26 @@
+package com.example.cscm94_gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class LoginController {
+    @FXML
+    public Button EnterButton;
+
+    @FXML
+    public void onEnterButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage) EnterButton.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("customer-profile.fxml"));
+        stage.setTitle("Customer Profile");
+        stage.getScene().setRoot(newRoot);
+    }
+
+
+}
--- CSCM94_GUI/src/main/java/com/example/cscm94_gui/OrderController.java
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/OrderController.java
@@ -4,12 +4,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.*;
-import javafx.scene.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class OrderController {
+
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    public void onLoginButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("log-in.fxml"));
+        stage.setTitle("Log in");
+        stage.getScene().setRoot(newRoot);
+    }
 
     @FXML
     public Button homeButton;
