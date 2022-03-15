e449390db5c38865c575a4f77f9963a88adaa216
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-15 15:45:57 +0000

    Last update

--- CSCM94_GUI/src/main/java/com/example/cscm94_gui/LogInController.java
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/LogInController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
--- CSCM94_GUI/src/main/java/com/example/cscm94_gui/RegisterController.java
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/RegisterController.java
@@ -1,4 +1,20 @@
 package com.example.cscm94_gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
 public class RegisterController {
+
+
+
+
 }
+
+
