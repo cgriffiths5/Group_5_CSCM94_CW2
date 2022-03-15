05fc202766c8e64bf56557433b51ad7c5cb8e83e
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-15 15:14:41 +0000

    Added ids

--- CSCM94_GUI/src/main/java/com/example/cscm94_gui/OrderController.java
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/OrderController.java
@@ -1,6 +1,13 @@
 package com.example.cscm94_gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class OrderController {
+
+    @FXML
+    public Button ConfirmOrderButton;
+
 
 
 }
