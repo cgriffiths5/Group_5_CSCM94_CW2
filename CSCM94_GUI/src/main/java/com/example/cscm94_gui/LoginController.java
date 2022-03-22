aafe275df662f7caaa31f43c23d46fa8446dec76
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-22 13:22:19 +0000

    Changed name of login file

--- CSCM94_GUI/src/main/java/com/example/cscm94_gui/LoginController.java
+++ CSCM94_GUI/src/main/java/com/example/cscm94_gui/LoginController.java
@@ -3,11 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.Parent;
 
 import java.io.IOException;
