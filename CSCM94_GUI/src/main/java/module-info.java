d4310368ddbd0316728f2644071e6d06d167a897
Adam Tucker <adamhtucker@live.co.uk>
2022-03-22 15:07:20 +0000

    Delete CSCM94_GUI directory

--- CSCM94_GUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cscm94_gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cscm94_gui to javafx.fxml;
-    exports com.example.cscm94_gui;
-}
