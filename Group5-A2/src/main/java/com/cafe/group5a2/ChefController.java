f5bd0c7ad0cd4c9017e6727728746b3db7ee7def
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-23 16:58:17 +0000

    Added home method

--- Group5-A2/src/main/java/com/cafe/group5a2/ChefController.java
+++ Group5-A2/src/main/java/com/cafe/group5a2/ChefController.java
@@ -1,4 +1,17 @@
 package com.cafe.group5a2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
 public class ChefController {
+
+    @FXML
+    public Button homeButton;
+
+    @FXML
+    public void onHomeButtonClick(ActionEvent event) {
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
 }
