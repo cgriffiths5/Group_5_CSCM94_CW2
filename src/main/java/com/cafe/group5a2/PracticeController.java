6b3f8d439a00513b278581636e3f339806400321
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-25 18:02:50 +0000

    File update

--- src/main/java/com/cafe/group5a2/PracticeController.java
+++
@@ -1,7 +0,0 @@
-package com.cafe.group5a2;
-
-import javafx.scene.control.ScrollPane;
-
-public class PracticeController {
-    public ScrollPane mainPane;
-}
--- src/main/java/com/cafe/group5a2/WaiterController.java
+++
@@ -1,7 +0,0 @@
-package com.cafe.group5a2;
-
-import javafx.scene.control.ComboBox;
-
-public class WaiterController {
-    public ComboBox combo;
-}
