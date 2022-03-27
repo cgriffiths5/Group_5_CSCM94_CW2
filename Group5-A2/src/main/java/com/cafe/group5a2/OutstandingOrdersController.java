1656a9ad24be5e97697e8376880455666fca1979
Chris Griffiths <chrisgriffiths635@outlook.com>
2022-03-27 12:11:18 +0100

    Added Outstanding Orders functionality

--- Group5-A2/src/main/java/com/cafe/group5a2/OutstandingOrdersController.java
+++ Group5-A2/src/main/java/com/cafe/group5a2/OutstandingOrdersController.java
@@ -1,26 +1,72 @@
 package com.cafe.group5a2;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class OutstandingOrdersController {
 
-    @FXML public Button ChefHomeReturn;
+    @FXML
+    public Button ChefHomeReturn;
+    public Button DisplayOrdersButton;
+    public Button SubmitCompleteOrder;
+    public TextField OrderID;
+
+    ObservableList<String> list = FXCollections.observableArrayList();
 
     //Database connection
     Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cafedb?user=root&password=");
 
     public OutstandingOrdersController() throws SQLException {
     }
+
+    public void displayOutstandingOrders() throws SQLException {
+        String query = "SELECT * FROM orders WHERE complete = 0;";
+
+        list.clear();
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next()) {
+                list.add(rs.getString("order_id") + " " + rs.getString("date_time") + " " + rs.getString("type") + "\n");
+            }
+
+            System.out.println(Arrays.asList(list.toArray()));
+
+        } catch (SQLException e) {
+            System.out.println("Error Detected");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onSubmitCompletedOrderButtonClick(ActionEvent actionEvent) throws SQLException {
+
+        String id = OrderID.getText();
+        int order_id = Integer.parseInt(id);
+
+        String query = "UPDATE orders SET complete = 1 WHERE order_ID = '" + order_id + "';";
+
+        try (Statement stmt = con.createStatement()) {
+             stmt.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println("Errors Detected");
+            e.printStackTrace();
+        }
+    }
+
+
 
     /**
      *
@@ -38,4 +84,6 @@ public class OutstandingOrdersController {
             System.out.println("Error loading page");
         }
     }
+
+
 }
