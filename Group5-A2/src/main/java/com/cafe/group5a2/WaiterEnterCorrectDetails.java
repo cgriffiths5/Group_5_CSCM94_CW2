1b2395169fbb81d1310190abc8bddaa640104bb4
CamTurner97 <91782242+CamTurner97@users.noreply.github.com>
2022-03-30 16:43:17 +0100

    adding java program to display wrong details

---
+++ Group5-A2/src/main/java/com/cafe/group5a2/WaiterEnterCorrectDetails.java
@@ -0,0 +1,26 @@
+package com.cafe.group5a2;
+
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WaiterEnterCorrectDetails implements Initializable {
+
+    public Button closeTab;
+
+    public void onClickCloseTab(ActionEvent actionEvent) {
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+}
+
+
